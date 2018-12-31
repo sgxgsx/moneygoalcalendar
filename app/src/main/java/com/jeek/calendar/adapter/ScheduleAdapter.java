0844be1668734766b2eeb7dffceb940b850acd29
vladosstrawberry <vladosstrawberry@gmail.com>
2018-12-31 04:06:42 +0200

    Icons/Design

--- app/src/main/java/com/jeek/calendar/adapter/ScheduleAdapter.java
+++ app/src/main/java/com/jeek/calendar/adapter/ScheduleAdapter.java
@@ -4,26 +4,19 @@ package com.jeek.calendar.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
-import android.os.AsyncTask;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.jeek.calendar.R;
 import com.jeek.calendar.activity.DetailEventActivity;
 import com.jimmy.common.CalendarSystemDatabase.Schedule;
-import com.jeek.calendar.fragment.ScheduleFragment;
-import com.jeek.calendar.task.schedule.RemoveScheduleTask;
-import com.jeek.calendar.task.schedule.UpdateScheduleTask;
 import com.jeek.calendar.utils.JeekUtils;
-import com.jeek.calendar.widget.StrikeThruTextView;
 import com.jimmy.common.base.app.BaseFragment;
-import com.jimmy.common.listener.OnTaskFinishedListener;
 
 import java.util.ArrayList;
 import java.util.List;
